0a1d09282cc81efccc04df0e4165eb5aef1aabdf
Christopher Toth <q.alpha@gmail.com>
2023-04-08 16:43:42 -0600

    Added a Toolbar and added ability to save the log.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import MudClient from "./client";
 import CommandInput from "./components/input";
 import { GMCPCore, GMCPCoreSupports, GMCPClientMedia } from "./gmcp";
 import { McpAwnsStatus, McpSimpleEdit } from "./mcp";
+import Toolbar from "./components/toolbar";
 
 const client = new MudClient("mongoose.moo.mud.org", 8765);
 client.registerGMCPPackage(GMCPCore);
@@ -16,13 +17,21 @@ client.registerMcpPackage(McpSimpleEdit);
 client.connect();
 
 function App() {
+  const outRef = React.useRef<OutputWindow | null>(null);
+  const saveLog = () => {
+    if (outRef.current) {
+      outRef.current.saveLog();
+    }
+  };
+
   useBeforeunload((event) => {
     client.shutdown();
   });
   return (
     <div className="App">
       <header className="App-header"></header>
-      <OutputWindow client={client} />
+      <Toolbar onSaveLog={saveLog} />
+      <OutputWindow client={client} ref={outRef} />
       <CommandInput onSend={(text: string) => client.sendCommand(text)} />
     </div>
   );
--- src/components/output.tsx
+++ src/components/output.tsx
@@ -1,158 +1,201 @@
-import './output.css';
+import "./output.css";
 // Output View for MUD Client
 import Anser, { AnserJsonEntry } from "anser";
-import * as React from 'react';
+import * as React from "react";
 
-import MudClient from '../client';
-
-
+import MudClient from "../client";
 
 interface Props {
-    client: MudClient;
+  client: MudClient;
 }
 
 interface State {
-    output: JSX.Element[];
+  output: JSX.Element[];
 }
 
 class Output extends React.Component<Props, State> {
-    outputRef: React.RefObject<HTMLDivElement> = React.createRef();
+  outputRef: React.RefObject<HTMLDivElement> = React.createRef();
 
-    state = {
-        output: [],
-    };
+  state = {
+    output: [],
+  };
 
-    constructor(props: Props) {
-        super(props);
-        this.props.client.on('message', this.handleMessage);
-        // connect
-        this.props.client.on('connect', () => this.addToOutput([<h2> Connected</h2>]));
-        // disconnect
-        this.props.client.on('disconnect', () => this.addToOutput([<h2> Disconnected</h2>]));
-        // error
-        this.props.client.on('error', (error: Error) => this.addToOutput([<h2> Error: {error.message}</h2>]));
+  constructor(props: Props) {
+    super(props);
+    this.props.client.on("message", this.handleMessage);
+    // connect
+    this.props.client.on("connect", () =>
+      this.addToOutput([<h2> Connected</h2>])
+    );
+    // disconnect
+    this.props.client.on("disconnect", () =>
+      this.addToOutput([<h2> Disconnected</h2>])
+    );
+    // error
+    this.props.client.on("error", (error: Error) =>
+      this.addToOutput([<h2> Error: {error.message}</h2>])
+    );
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
+  componentWillUnmount() {
+    this.props.client.removeListener("message", this.handleMessage);
+  }
+
+  addToOutput(elements: any[]) {
+    this.setState((state) => {
+      // console.log("Current output length: " + state.output.length)
+      const key = state.output.length;
+      const newOutput = elements.map((element, index) => (
+        <div key={key + index}>{element}</div>
+      ));
+      return { output: [...state.output, ...newOutput] };
+    });
+  }
+
+  scrollToBottom = () => {
+    const output = this.outputRef.current;
+    if (output) {
+      output.scrollTop = output.scrollHeight;
     }
+  };
 
-    componentDidUpdate() {
-        this.scrollToBottom();
+  handleMessage = (message: string) => {
+    const elements = parseToElements(message, this.handleExitClick);
+    this.addToOutput(elements);
+  };
+
+  handleExitClick = (exit: string) => {
+    this.props.client.sendCommand(exit);
+  };
+
+  saveLog() {
+    const output = this.outputRef.current;
+    if (output) {
+      // we want to save as HTML to preserve ansi and links
+      const html = output.innerHTML;
+      const blob = new Blob([html], { type: "text/html" });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      // include the date and time
+      const date = new Date();
+      const dateString = date.toLocaleDateString();
+      const timeString = date.toLocaleTimeString();
+      const filename = `mud-log-${dateString}-${timeString}.html`;
+      link.download = filename;
+      link.href = url;
+      link.click();
+      // cleanup
+      URL.revokeObjectURL(url);
+      link.remove();
     }
+  }
 
-    componentWillUnmount() {
-        this.props.client.removeListener('message', this.handleMessage);
-    }
-
-    addToOutput(elements: any[]) {
-        this.setState((state) => {
-            // console.log("Current output length: " + state.output.length)
-            const key = state.output.length;
-            const newOutput = elements.map((element, index) => <div key={key + index}>{element}</div>);
-            return { output: [...state.output, ...newOutput] };
-        });
-    }
-
-    scrollToBottom = () => {
-        const output = this.outputRef.current;
-        if (output) {
-            output.scrollTop = output.scrollHeight;
-        }
-    };
-
-    handleMessage = (message: string) => {
-        const elements = parseToElements(message, this.handleExitClick);
-        this.addToOutput(elements);
-    };
-
-    handleExitClick = (exit: string) => {
-        this.props.client.sendCommand(exit);
-    }
-
-    render() {
-        return (
-            <div ref={this.outputRef} className="output" aria-live="polite" role="log">
-                {this.state.output}
-            </      div>
-        );
-    }
+  render() {
+    return (
+      <div
+        ref={this.outputRef}
+        className="output"
+        aria-live="polite"
+        role="log"
+      >
+        {this.state.output}
+      </div>
+    );
+  }
 }
 
 export default Output;
 
-
-export function parseToElements(text: string, onExitClick: (exit: string) => void): React.ReactNode[] {
-    let elements: React.ReactNode[] = [];
-    // handle multiline strings by splitting them and adding the appropriate <br/>
-    for (const line of text.split('\r\n')) {
-        const parsed = Anser.ansiToJson(line, { json: true, remove_empty: false });
-        let children: any[] = [];
-        for (const bundle of parsed) {
-            const newElements = convertBundleIntoReact(bundle, onExitClick);
-            children = [...children, ...newElements]
-        }
-        elements = [...elements, <span key={elements.length}>{children}</span>];
+export function parseToElements(
+  text: string,
+  onExitClick: (exit: string) => void
+): React.ReactNode[] {
+  let elements: React.ReactNode[] = [];
+  // handle multiline strings by splitting them and adding the appropriate <br/>
+  for (const line of text.split("\r\n")) {
+    const parsed = Anser.ansiToJson(line, { json: true, remove_empty: false });
+    let children: any[] = [];
+    for (const bundle of parsed) {
+      const newElements = convertBundleIntoReact(bundle, onExitClick);
+      children = [...children, ...newElements];
     }
-    return elements;
+    elements = [...elements, <span key={elements.length}>{children}</span>];
+  }
+  return elements;
 }
 
-const URL_REGEX = /(\s|^)(https?:\/\/(?:www\.|(?!www))[^\s.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/g;
-const EMAIL_REGEX = /(\s|^)[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+(\s|$)/g;
+const URL_REGEX =
+  /(\s|^)(https?:\/\/(?:www\.|(?!www))[^\s.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/g;
+const EMAIL_REGEX =
+  /(\s|^)[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+(\s|$)/g;
 const exitRegex = /@\[exit:([a-zA-Z]+)\]([a-zA-Z]+)@\[\/\]/g;
 
-function convertBundleIntoReact(bundle: AnserJsonEntry, onExitClick: (exit: string) => void): React.ReactNode[] {
-    const style = createStyle(bundle);
-    const content: React.ReactNode[] = [];
-    let index = 0;
+function convertBundleIntoReact(
+  bundle: AnserJsonEntry,
+  onExitClick: (exit: string) => void
+): React.ReactNode[] {
+  const style = createStyle(bundle);
+  const content: React.ReactNode[] = [];
+  let index = 0;
 
-    function processRegex(regex: RegExp, process: (match: RegExpExecArray) => React.ReactNode): void {
-        let match: RegExpExecArray | null;
-        while ((match = regex.exec(bundle.content)) !== null) {
-            const startIndex = match.index;
-            if (startIndex > index) {
-                content.push(bundle.content.substring(index, startIndex));
-            }
-            content.push(process(match));
-            index = regex.lastIndex;
-        }
+  function processRegex(
+    regex: RegExp,
+    process: (match: RegExpExecArray) => React.ReactNode
+  ): void {
+    let match: RegExpExecArray | null;
+    while ((match = regex.exec(bundle.content)) !== null) {
+      const startIndex = match.index;
+      if (startIndex > index) {
+        content.push(bundle.content.substring(index, startIndex));
+      }
+      content.push(process(match));
+      index = regex.lastIndex;
     }
+  }
 
-    function processUrlMatch(match: RegExpExecArray): React.ReactNode {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const [, pre, url] = match;
-        const href = url;
-        return (
-            <a href={href} target="_blank" rel="noreferrer">
-                {url}
-            </a>
-        );
-    }
+  function processUrlMatch(match: RegExpExecArray): React.ReactNode {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [, pre, url] = match;
+    const href = url;
+    return (
+      <a href={href} target="_blank" rel="noreferrer">
+        {url}
+      </a>
+    );
+  }
 
-    function processEmailMatch(match: RegExpExecArray): React.ReactNode {
-        const email = match[0];
-        const href = `mailto:${email}`;
-        return (
-            <a href={href} target="_blank" rel="noreferrer">
-                {email}
-            </a>
-        );
-    }
+  function processEmailMatch(match: RegExpExecArray): React.ReactNode {
+    const email = match[0];
+    const href = `mailto:${email}`;
+    return (
+      <a href={href} target="_blank" rel="noreferrer">
+        {email}
+      </a>
+    );
+  }
 
-    function processExitMatch(match: RegExpExecArray): React.ReactNode {
-        const [, exitType, exitName] = match;
-        return (
-            // eslint-disable-next-line jsx-a11y/anchor-is-valid
-            <a onClick={() => onExitClick(exitType)} className="exit">
-                {exitName}
-            </a>
-        );
-    }
+  function processExitMatch(match: RegExpExecArray): React.ReactNode {
+    const [, exitType, exitName] = match;
+    return (
+      // eslint-disable-next-line jsx-a11y/anchor-is-valid
+      <a onClick={() => onExitClick(exitType)} className="exit">
+        {exitName}
+      </a>
+    );
+  }
 
-    processRegex(URL_REGEX, processUrlMatch);
-    processRegex(EMAIL_REGEX, processEmailMatch);
-    processRegex(exitRegex, processExitMatch);
+  processRegex(URL_REGEX, processUrlMatch);
+  processRegex(EMAIL_REGEX, processEmailMatch);
+  processRegex(exitRegex, processExitMatch);
 
-    if (index < bundle.content.length) {
-        content.push(bundle.content.substring(index));
-    }
-    return content.map((c) => <span style={style}>{c}</span>);
+  if (index < bundle.content.length) {
+    content.push(bundle.content.substring(index));
+  }
+  return content.map((c) => <span style={style}>{c}</span>);
 }
 
 /**
@@ -163,38 +206,37 @@ function convertBundleIntoReact(bundle: AnserJsonEntry, onExitClick: (exit: stri
  * @return {Object} returns the style object
  */
 function createStyle(bundle: AnserJsonEntry): React.CSSProperties {
-    const style: React.CSSProperties = {};
-    if (bundle.bg) {
-        style.backgroundColor = `rgb(${bundle.bg})`;
-    }
-    if (bundle.fg) {
-        style.color = `rgb(${bundle.fg})`;
-    }
-    switch (bundle.decoration) {
-        case 'bold':
-            style.fontWeight = 'bold';
-            break;
-        case 'dim':
-            style.opacity = '0.5';
-            break;
-        case 'italic':
-            style.fontStyle = 'italic';
-            break;
-        case 'hidden':
-            style.visibility = 'hidden';
-            break;
-        case 'strikethrough':
-            style.textDecoration = 'line-through';
-            break;
-        case 'underline':
-            style.textDecoration = 'underline';
-            break;
-        case 'blink':
-            style.textDecoration = 'blink';
-            break;
-        default:
-            break;
-    }
-    return style;
+  const style: React.CSSProperties = {};
+  if (bundle.bg) {
+    style.backgroundColor = `rgb(${bundle.bg})`;
+  }
+  if (bundle.fg) {
+    style.color = `rgb(${bundle.fg})`;
+  }
+  switch (bundle.decoration) {
+    case "bold":
+      style.fontWeight = "bold";
+      break;
+    case "dim":
+      style.opacity = "0.5";
+      break;
+    case "italic":
+      style.fontStyle = "italic";
+      break;
+    case "hidden":
+      style.visibility = "hidden";
+      break;
+    case "strikethrough":
+      style.textDecoration = "line-through";
+      break;
+    case "underline":
+      style.textDecoration = "underline";
+      break;
+    case "blink":
+      style.textDecoration = "blink";
+      break;
+    default:
+      break;
+  }
+  return style;
 }
-
