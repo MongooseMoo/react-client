2e78fe747d7bf04b81a8fb51bfbf17dd11ef4ec9
Christopher Toth <q.alpha@gmail.com>
2023-04-28 22:54:57 -0600

    audio chat (#3)
    * Basic audio chat using Livekit
    
    * Better state management for when people enter/leave
    
    * Use default Livekit theme
    
    * audio chat display fixes
    
    * audio chat display changes
    
    ---------
    
    Co-authored-by: thekettleison <121907631+thekettleison@users.noreply.github.com>

---
+++ src/components/audioChat.tsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { AudioConference, LiveKitRoom } from '@livekit/components-react';
+import MudClient from '../client';
+
+
+const serverUrl = 'wss://mongoose-67t79p35.livekit.cloud';
+
+interface AudioChatProps {
+
+    client: MudClient;
+}
+
+interface AudioChatState {
+    connected: boolean;
+    token: string;
+}
+
+class AudioChat extends Component<AudioChatProps, AudioChatState> {
+
+    constructor(props: AudioChatProps) {
+        super(props);
+        this.state = {
+            token: '',
+            connected: false,
+        };
+    }
+
+    componentDidMount() {
+        const { client } = this.props;
+
+        client.on('livekitToken', (token) => {
+            this.setState({ connected: false, token: token }, () => {
+                this.setState({ connected: true });
+            });
+        });
+    }
+
+    render() {
+        const { token } = this.state;
+
+        return (
+            <div data-lk-theme="default">
+                <LiveKitRoom
+                    video={false}
+                    audio={true}
+                    token={token}
+                    serverUrl={serverUrl}
+                    connect={this.state.connected}
+                >
+                    <AudioConference />
+                </LiveKitRoom>
+            </div>
+        );
+    }
+}
+
+export default AudioChat;
