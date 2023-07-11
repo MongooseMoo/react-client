435e566cedbd50e5407ccfb203be83fae97ae0bf
Christopher Toth <q.alpha@gmail.com>
2023-07-10 23:43:22 -0600

    Work towards multiple livekit calls

--- src/components/audioChat.tsx
+++ src/components/audioChat.tsx
@@ -9,45 +9,56 @@ interface AudioChatProps {
 }
 
 const AudioChat: React.FC<AudioChatProps> = ({ client }) => {
-  const [token, setToken] = useState("");
+  const [tokens, setTokens] = useState<string[]>([]);
   const [connected, setConnected] = useState(false);
 
   useEffect(() => {
     const handleLiveKitToken = (token: string) => {
       setConnected(false);
-      setToken(token);
+      setTokens(prevTokens => [...prevTokens, token]);
       setTimeout(() => {
         setConnected(true);
       }, 1000);
     };
 
+    const handleLiveKitLeave = (token: string) => {
+      setTokens(prevTokens => {
+        const updatedTokens = prevTokens.filter(prevToken => prevToken !== token);
+        if (updatedTokens.length === 0) {
+          setConnected(false);
+        }
+        return updatedTokens;
+      });
+    };
+
     client.on("livekitToken", handleLiveKitToken);
+    client.on("livekitLeave", handleLiveKitLeave);
 
     return () => {
       client.off("livekitToken", handleLiveKitToken);
+      client.off("livekitLeave", handleLiveKitLeave);
     };
   }, [client]);
 
-  const handleDisconnected = () => {
-    setToken("");
-  };
-
-  if (!token) {
-    return null; // Returning null here instead of empty string
+  if (!tokens.length) {
+    return null;
   }
 
   return (
     <div data-lk-theme="default">
-      <LiveKitRoom
-        video={false}
-        audio={true}
-        token={token}
-        serverUrl={serverUrl}
-        connect={connected}
-        onDisconnected={handleDisconnected}
-      >
-        <AudioConference />
-      </LiveKitRoom>
+      {tokens.map((token, index) => (
+        <LiveKitRoom
+          key={index}
+          video={false}
+          audio={true}
+          token={token}
+          serverUrl={serverUrl}
+          connect={connected}
+          onDisconnected={() => client.emit("livekitLeave", token)}
+        >
+          <AudioConference />
+        </LiveKitRoom>
+      ))}
       <div className="audio-status" aria-live="polite"></div>
     </div>
   );
