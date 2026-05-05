12fb7f504f4178230bdd3d946c6a6af24e3baa42
ctoth <ctoth@users.noreply.github.com>
2026-05-05 00:30:28 +0000

    Always announce talker and message 35fa1dee72e783d101e7255b0fb1d224610bfc10

--- assets/index-CtC0414q.js
+++ assets/index-CtC0414q.js
@@ -1,4 +1,4 @@
-import{r as Ne,g as Rt,c as Xe}from"./index-DaIVmerN.js";function It(e,n){for(var i=0;i<n.length;i++){const t=n[i];if(typeof t!="string"&&!Array.isArray(t)){for(const u in t)if(u!=="default"&&!(u in e)){const d=Object.getOwnPropertyDescriptor(t,u);d&&Object.defineProperty(e,u,d.get?d:{enumerable:!0,get:()=>t[u]})}}}return Object.freeze(Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}))}var _e={},ne={},le={},Ze;function lt(){if(Ze)return le;Ze=1;/*!
+import{r as Ne,g as Rt,c as Xe}from"./index-ZRMAe9Px.js";function It(e,n){for(var i=0;i<n.length;i++){const t=n[i];if(typeof t!="string"&&!Array.isArray(t)){for(const u in t)if(u!=="default"&&!(u in e)){const d=Object.getOwnPropertyDescriptor(t,u);d&&Object.defineProperty(e,u,d.get?d:{enumerable:!0,get:()=>t[u]})}}}return Object.freeze(Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}))}var _e={},ne={},le={},Ze;function lt(){if(Ze)return le;Ze=1;/*!
  * Buttplug JS Source Code File - Visit https://buttplug.io for more info about
  * the project. Licensed under the BSD 3-Clause license. See LICENSE file in the
  * project root for full license information.
